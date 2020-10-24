371c6129d44adffaa38cad9b04a28089abccb3d2
heehee812 <e.enyuu812@gmail.com>
2020-10-25 01:15:39 +0800

    line send location

--- httpp.py
+++ httpp.py
@@ -37,9 +37,10 @@ crl.close()
 
 # Get the content stored in the BytesIO object (in byte characters) 
 get_body = b_obj.getvalue()
+message= get_body.decode('utf8')
 
 # Decode the bytes stored in get_body to HTML and print the result 
-print('Output of GET request:\n%s' % get_body.decode('utf8')) 
+print('Output of GET request:\n%s' % message) 
 
 
 # # line push message
@@ -55,3 +56,15 @@ print('Output of GET request:\n%s' % get_body.decode('utf8'))
 #     pass
 
 # location
+
+# line push message
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from linebot.exceptions import LineBotApiError
+
+line_bot_api = LineBotApi('O8kuFrVBCMH6j4TcxbtKSqTWzzgBfejuvKgsn0yosH1CMb7g7NRZdJPorxOENF+y1GGKbx9+Km2Ffc4x/S/yPACFLtr9FveKo45X17kgDVfWmezJb8PNJ08JwZJc454oss6+fknmEssQskCs8tXTaQdB04t89/1O/w1cDnyilFU=')
+
+try:
+    line_bot_api.push_message('U6b0f8d723a86579eda979e83510d36f1', TextSendMessage(text=message))
+except LineBotApiError as e:
+    pass

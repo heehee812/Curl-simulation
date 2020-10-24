e4c10469e40fcf42dd4a2b3ded98df97e8b1d47d
heehee812 <e.enyuu812@gmail.com>
2020-10-25 01:20:05 +0800

    debug

--- httpp.py
+++ httpp.py
@@ -37,10 +37,9 @@ crl.close()
 
 # Get the content stored in the BytesIO object (in byte characters) 
 get_body = b_obj.getvalue()
-message= get_body.decode('utf8')
 
 # Decode the bytes stored in get_body to HTML and print the result 
-print('Output of GET request:\n%s' % message) 
+print('Output of GET request:\n%s' % get_body.decode('utf8')) 
 
 
 # # line push message
@@ -65,6 +64,6 @@ from linebot.exceptions import LineBotApiError
 line_bot_api = LineBotApi('O8kuFrVBCMH6j4TcxbtKSqTWzzgBfejuvKgsn0yosH1CMb7g7NRZdJPorxOENF+y1GGKbx9+Km2Ffc4x/S/yPACFLtr9FveKo45X17kgDVfWmezJb8PNJ08JwZJc454oss6+fknmEssQskCs8tXTaQdB04t89/1O/w1cDnyilFU=')
 
 try:
-    line_bot_api.push_message('U6b0f8d723a86579eda979e83510d36f1', TextSendMessage(text=message))
+    line_bot_api.push_message('U6b0f8d723a86579eda979e83510d36f1', TextSendMessage(text='Hello World!'))
 except LineBotApiError as e:
     pass
